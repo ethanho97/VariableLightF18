c93cf5fba13c709526b0003bc192884e37b03c88
Ethan Ho <eh154@duke.edu>
2018-12-13 07:01:44 -0500

    Updated methods and variable names so this is no longer pseudo-code

--- Software/software.ino
+++ Software/software.ino
@@ -1,7 +1,7 @@
 // use preprocessor directives to define constants in the code
 #define FLASH_RATE_HZ 2
-#define LED_PIN D4
-#define BUTTON_PIN D1
+#define BUTTON_PIN 4
+#define LED_PIN 12
 #define PWN_LED_OUT D11
 #define PWM_MAX 255
 #define NUM_MODE 5
@@ -9,31 +9,28 @@
 // global variables representing "state" across all functions
 // operating mode: 0 - off, 1 - bright, 2 - mid-bright, 3 - dim, 4 - flash
 int operating_mode = 0;
-bool button_pushed = TRUE;
+bool button_press = false;
 int previous_button_state = 0;
 int PWM_OUT = 0;
 
 // executed one-time at device startup
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(BUTTON_IN, button_pushed, FALLING);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_pushed, FALLING);
    // define output (PWM) pin connected to LED
-
 }
 
 // continually-running loop
 // calls functions that are named as "actions"
 void loop(){
-
-    check_for_button_press();
-
-    set_pwm_based_on_operating_mode();
-
-    shine_led();
-
+    check_button_press();
+    set_pwm();
+    shine_led(PWM_OUT);
 }
 
-void set_pwn_based_on_operating_mode() {
+void set_pwm() {
 
     switch (operating_mode) {
         case 0:
@@ -41,38 +38,36 @@ void set_pwn_based_on_operating_mode() {
         case 1:
             PWM_OUT = PWM_MAX;
         case 2:
-            PWN_OUT = int(PWM_MAX/2);
+            PWM_OUT = int(PWM_MAX/2);
         case 3:
-            PWN_OUT = int(PWM_MAX/4);
+            PWM_OUT = int(PWM_MAX/4);
         case 4:
+            PWM_OUT = PWM_MAX;
             flash_the_light();
     }
 
 }
 
 void button_pushed() {
-    BUTTON_PUSHED = TRUE;
+    button_press = true;
 }
 
 void flash_the_light() {
-    digitalOutput(magic_in_here);
+    analogWrite(LED_PIN, PWM_OUT);
     delay(1000);
 }
 
-void shine_led(PWN_OUT) {
-    digitalOutput(magic_in_here);
+void shine_led(int PWN) {
+    analogWrite(LED_PIN, PWM_OUT);
 }
 
 void check_button_press() {
-    // set operating_mode
-    // reset previous button press state, interrupt catch, etc.
-
-    // initialize button some way
-    if (button){
-        if (operating_mode > NUM_MODES){
-            operating_mode = 0
+    if (button_press){
+        if (operating_mode > NUM_MODE - 1){
+            operating_mode = 0;
         }else {
-            operating_mode++
+            operating_mode++;
         }
+        button_press = false;
     }
 }
