08d0753e46a38ed8cd1ccef4ff23a42dcc7a3fc1
Ethan Ho <eh154@duke.edu>
2018-12-13 18:52:33 -0500

    Fixed setting correct operation mode and added debouncing to buttons

--- Software/software.ino
+++ Software/software.ino
@@ -1,73 +1,84 @@
 // use preprocessor directives to define constants in the code
 #define FLASH_RATE_HZ 2
-#define BUTTON_PIN 4
-#define LED_PIN 12
-#define PWN_LED_OUT D11
+#define BUTTON_PIN 3
+#define LED_PIN 11
 #define PWM_MAX 255
 #define NUM_MODE 5
 
-// global variables representing "state" across all functions
-// operating mode: 0 - off, 1 - bright, 2 - mid-bright, 3 - dim, 4 - flash
 int operating_mode = 0;
 bool button_press = false;
-int previous_button_state = 0;
 int PWM_OUT = 0;
 
-// executed one-time at device startup
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_pushed, FALLING);
-   // define output (PWM) pin connected to LED
 }
 
-// continually-running loop
-// calls functions that are named as "actions"
 void loop(){
-    check_button_press();
-    set_pwm();
-    shine_led(PWM_OUT);
+  check_button_press();
+  set_pwm();
 }
 
 void set_pwm() {
-
-    switch (operating_mode) {
-        case 0:
-            PWM_OUT = 0;
-        case 1:
-            PWM_OUT = PWM_MAX;
-        case 2:
-            PWM_OUT = int(PWM_MAX/2);
-        case 3:
-            PWM_OUT = int(PWM_MAX/4);
-        case 4:
-            PWM_OUT = PWM_MAX;
-            flash_the_light();
+  switch (operating_mode) {
+    case 0:
+      PWM_OUT = 0;
+      shine_led(PWM_OUT);
+//      Serial.println(PWM_OUT);
+      break;
+    case 1:
+      PWM_OUT = PWM_MAX;
+      shine_led(PWM_OUT);
+//      Serial.println(PWM_OUT);
+      break;
+    case 2:
+      PWM_OUT = int(PWM_MAX/2);
+      shine_led(PWM_OUT);
+//      Serial.println(PWM_OUT);
+      break;
+    case 3:
+      PWM_OUT = int(PWM_MAX/4);
+      shine_led(PWM_OUT);
+//      Serial.println(PWM_OUT);
+      break;
+    case 4:
+//      Serial.println(PWM_OUT);
+      flash_the_light();
+      break;
     }
-
 }
 
 void button_pushed() {
+  if ( (millis() - lastDebounceTime) > debounceDelay) {
     button_press = true;
+    lastDebounceTime = millis();
+    Serial.println(operating_mode);
+  }
 }
 
 void flash_the_light() {
-    analogWrite(LED_PIN, PWM_OUT);
-    delay(1000);
+  analogWrite(LED_PIN, PWM_MAX);
+  delay(1000/FLASH_RATE_HZ);
+  analogWrite(LED_PIN, 0);
+  delay(1000/FLASH_RATE_HZ);
 }
 
-void shine_led(int PWN) {
-    analogWrite(LED_PIN, PWM_OUT);
+void shine_led(int PWM) {
+  analogWrite(LED_PIN, PWM);
 }
 
 void check_button_press() {
-    if (button_press){
-        if (operating_mode > NUM_MODE - 1){
-            operating_mode = 0;
-        }else {
-            operating_mode++;
-        }
-        button_press = false;
+  if (button_press){
+    if (operating_mode == NUM_MODE - 1){
+      operating_mode = 0;
+    }else {
+      operating_mode++;
     }
+      button_press = false;
+  }
 }
